806ffb78fc6379da0b162c8aae7467c59de16d3f
mlbd <mlimonbd@gmail.com>
2024-01-29 22:28:20 +0600

    fix inidex.js content

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
-    response.setHeader('content-type', 'text/html; charset=utf-8');
-    response.send(`
+    res.setHeader('content-type', 'text/html; charset=utf-8');
+    res.send(`
         404 Error: Whoops! Looks like this route took a wrong turn and ended up in the land of forgotten endpoints. We tried sending a search party, but they got lost too. Maybe it's time to use a GPS for your API calls? 🗺️ #LostInCyberspace
     `);
 };
