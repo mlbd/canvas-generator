c63af540ae09700bd3791ba43cb6e9839e5b2a3e
mlbd <mlimonbd@gmail.com>
2024-01-29 22:25:41 +0600

    change index.js content

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-404 Error: Whoops! Looks like this route took a wrong turn and ended up in the land of forgotten endpoints. We tried sending a search party, but they got lost too. Maybe it's time to use a GPS for your API calls? 🗺️ #LostInCyberspace
+module.exports = async (req, res) => {
+    response.setHeader('content-type', 'text/html; charset=utf-8');
+    response.send(`
+        404 Error: Whoops! Looks like this route took a wrong turn and ended up in the land of forgotten endpoints. We tried sending a search party, but they got lost too. Maybe it's time to use a GPS for your API calls? 🗺️ #LostInCyberspace
+    `);
+};
