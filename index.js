f5f410f0c73b97a63ec93b62228a80864e4995ec
mlbd <mlimonbd@gmail.com>
2024-01-29 21:54:21 +0600

    change index.js content

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-module.exports = async (req, res) => {
-    res.status(200).json({ 
-        message: "404 Error: Whoops! Looks like this route took a wrong turn and ended up in the land of forgotten endpoints. We tried sending a search party, but they got lost too. Maybe it's time to use a GPS for your API calls? 🗺️ #LostInCyberspace",
-        body: 'req.body',
-        method: req.method,
-        url: req.url,
-        query: req.query
-    });
-};
+<h2>404 Error: Whoops! Looks like this route took a wrong turn and ended up in the land of forgotten endpoints. We tried sending a search party, but they got lost too. Maybe it's time to use a GPS for your API calls? 🗺️ #LostInCyberspace</h2>
